586bd2d90a44a6f480ab392b62fc90efad4f79f5
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-10-10 19:07:36 +0600

    fix the domperser issu

--- src/hooks/usePublic.jsx
+++ src/hooks/usePublic.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000/", // replace with your API base URL
+  // baseURL: "http://localhost:3000", // replace with your API base URL
+  baseURL: "https://reduan-portfolio.vercel.app", // replace with your API base URL
 });
 const usePublic = () => {
   return axiosPublic;
