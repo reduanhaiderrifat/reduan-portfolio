68621e0b91fe730a2a99afd84e7a1296b687eb9c
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-10-12 01:03:32 +0600

    scure the cookies

--- src/lib/Session.js
+++
@@ -1,7 +0,0 @@
-import { useSession } from "next-auth/react";
-
-export default Session = () => {
-  const session = useSession();
-  const email = session?.data?.user?.email;
-  return email;
-};
--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 export const middleware = async (request) => {
-  const token = cookies(request).get("next-auth.session-token");
+  const token = cookies(request).get("__Secure-next-auth.session-token");
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
